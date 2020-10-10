1369d6d6c05a5898231b70cdd6de089c23bb80e5
Vlad Karmazin <37839328+kain69@users.noreply.github.com>
2020-10-10 15:56:37 +0500

    Доделал SendMessage и Распределенную память

--- Game2/Source.cpp
+++ Game2/Source.cpp
@@ -13,7 +13,7 @@
 #define KEY_SHIFTED     0x8000
 #define KEY_TOGGLED     0x0001
 
-const TCHAR* szClsName = _T("HelloWindowClass123456789");
+const TCHAR* szClsName = _T("HelloWindowClass");
 //----------------------------------------------------------------------------------------
 struct rgb {
 	double r, g, b;
@@ -28,8 +28,9 @@ struct Config {
 
 }cfg;
 
+UINT WM_UPDATE_MESSAGE = RegisterWindowMessage(0);
 int** array;
-std::string scfg = "config.cfg";
+std::string scfg = "P:\\Влад\\ВУЗ\\2 курс\\ОС\\Game2\\config.cfg";
 //----------------------------------------------------------------------------------------
 void rgbChanger2(rgb& color, double delta)
 {
@@ -144,6 +145,23 @@ HPEN		hPen;
 PAINTSTRUCT ps;
 HDC			hdc;
 
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lParam)
+{
+	TCHAR szTextWin[255];
+	if (GetWindowText(hwnd, (LPWSTR)szTextWin, sizeof(szTextWin)))
+	{
+		//|| wcscmp(szTextWin, TEXT("P:\\Влад\\ВУЗ\\2 курс\\ОС\\Game2\\Debug\\Game2.exe")) == 0
+		if (wcscmp(szTextWin, TEXT("HelloWindow")) == 0)
+		{
+			std::cout << "hwnd: " << hwnd << "\n" << "name: ";
+			_tprintf(szTextWin);
+			std::cout << "\n";
+			SendMessage(hwnd, WM_UPDATE_MESSAGE, NULL, NULL);
+		}
+	}
+	return TRUE;
+}
+
 LRESULT CALLBACK WndProc(
 	HWND	hWnd,
 	UINT	message,
@@ -159,200 +177,209 @@ LRESULT CALLBACK WndProc(
 	int b = (lprect->bottom - lprect->top) / cfg.n;
 	int x, y;
 	HBRUSH col = NULL;
-	switch (message)
+
+	if (message == WM_UPDATE_MESSAGE) {
+		InvalidateRect(hWnd, lprect, 1);
+	}
+	else
 	{
-
-	case WM_RBUTTONUP:
-		x = GET_X_LPARAM(lParam); y = GET_Y_LPARAM(lParam);
-		for (int i = 0; i < cfg.n; i++)
+		switch (message)
 		{
-			for (int j = 0; j < cfg.n; j++)
+
+		case WM_RBUTTONUP:
+			x = GET_X_LPARAM(lParam); y = GET_Y_LPARAM(lParam);
+			for (int i = 0; i < cfg.n; i++)
 			{
-				int xl = a	* i;
-				int xr = xl + a;
-				int yt = b	* j;
-				int yb = yt + b;
-
-				if ((xl <= x && yt <= y) && (xr > x && yb > y))
+				for (int j = 0; j < cfg.n; j++)
 				{
-					if (array[i][j] == 2)
-					{
-						array[i][j] = 0;
-					}
-					else
-						array[i][j] = 2;
-					break;
-				}
-			}
-		}
-		InvalidateRect(hWnd, lprect, 1);
-		return 0;
-
-	case WM_LBUTTONUP:
-
-		x = GET_X_LPARAM(lParam); y = GET_Y_LPARAM(lParam);
-		for (int i = 0; i < cfg.n; i++)
-		{
-			for (int j = 0; j < cfg.n; j++)
-			{
-				int xl = a	* i;
-				int xr = xl + a;
-				int yt = b	* j;
-				int yb = yt + b;
-
-				if ((xl <= x && yt <= y) && (xr > x && yb > y))
-				{
-					if (array[i][j] == 1)
-					{
-						array[i][j] = 0;
-					}
-					else
-						array[i][j] = 1;
-					break;
-				}
-			}
-		}
-		InvalidateRect(hWnd, lprect, 1);
-		return 0;
-
-	case WM_PAINT:
-
-		hdc = BeginPaint(hWnd, &ps);
-		for (int i = 0; i < cfg.n; i++)
-		{
-			for (int j = 0; j < cfg.n; j++)
-			{
-
-				if (array[i][j] == 1)
-				{
-					HDC hdc = GetDC(hWnd);
-					HBRUSH hBrush = CreateSolidBrush(RGB(100, 100, 100));
-					SelectObject(hdc, hBrush);
-					int xl = a	* i;
+					int xl = a * i;
 					int xr = xl + a;
-					int yt = b	* j;
-					int yb = yt	+ b;
-					Ellipse(hdc,	xl + a / 20 + 3,	yt + a / 20, xr - a / 20 - 3,	yb - a / 20);
-					DeleteObject(hBrush);
-					DeleteObject(hdc);
-				}
-				if (array[i][j] == 2)
-				{
-					HDC hdc = GetDC(hWnd);
-					hPen = CreatePen(PS_SOLID,	4,	RGB(255, 255, 255));
-					SelectObject(hdc, hPen);
-					int xl = a	* i;
-					int xr = xl + a;
-					int yt = b	* j;
+					int yt = b * j;
 					int yb = yt + b;
 
-					MoveToEx(hdc,	xl + a - a / 10,	yb - a / 10,	0);
-					LineTo	(hdc,	xr - a + a / 10,	yt + a / 10);
-
-					MoveToEx(hdc,	xl + a / 10,		yb - a / 10,	0);
-					LineTo	(hdc,	xr - a / 10,		yt + a / 10);
-
-					DeleteObject(hPen);
-					DeleteObject(hdc);
+					if ((xl <= x && yt <= y) && (xr > x && yb > y))
+					{
+						if (array[i][j] == 2)
+						{
+							array[i][j] = 0;
+						}
+						else
+							array[i][j] = 2;
+						break;
+					}
 				}
+			}
+			
+			EnumWindows(&EnumWindowsProc, 0);
+			InvalidateRect(hWnd, lprect, 1);
+			return 0;
+
+		case WM_LBUTTONUP:
+		{
+			x = GET_X_LPARAM(lParam); y = GET_Y_LPARAM(lParam);
+			for (int i = 0; i < cfg.n; i++)
+			{
+				for (int j = 0; j < cfg.n; j++)
+				{
+					int xl = a * i;
+					int xr = xl + a;
+					int yt = b * j;
+					int yb = yt + b;
+
+					if ((xl <= x && yt <= y) && (xr > x && yb > y))
+					{
+						if (array[i][j] == 1)
+						{
+							array[i][j] = 0;
+						}
+						else
+							array[i][j] = 1;
+						break;
+					}
+				}
+			}
+			EnumWindows(&EnumWindowsProc, 0);
+			InvalidateRect(hWnd, lprect, 1);
+			return 0;
+		}
+		case WM_PAINT:
+			hdc = BeginPaint(hWnd, &ps);
+			for (int i = 0; i < cfg.n; i++)
+			{
+				for (int j = 0; j < cfg.n; j++)
+				{
+
+					if (array[i][j] == 1)
+					{
+						HDC hdc = GetDC(hWnd);
+						HBRUSH hBrush = CreateSolidBrush(RGB(100, 100, 100));
+						SelectObject(hdc, hBrush);
+						int xl = a * i;
+						int xr = xl + a;
+						int yt = b * j;
+						int yb = yt + b;
+						Ellipse(hdc, xl + a / 20 + 3, yt + a / 20, xr - a / 20 - 3, yb - a / 20);
+						DeleteObject(hBrush);
+						DeleteObject(hdc);
+					}
+					if (array[i][j] == 2)
+					{
+						HDC hdc = GetDC(hWnd);
+						hPen = CreatePen(PS_SOLID, 4, RGB(255, 255, 255));
+						SelectObject(hdc, hPen);
+						int xl = a * i;
+						int xr = xl + a;
+						int yt = b * j;
+						int yb = yt + b;
+
+						MoveToEx(hdc, xl + a - a / 10, yb - a / 10, 0);
+						LineTo(hdc, xr - a + a / 10, yt + a / 10);
+
+						MoveToEx(hdc, xl + a / 10, yb - a / 10, 0);
+						LineTo(hdc, xr - a / 10, yt + a / 10);
+
+						DeleteObject(hPen);
+						DeleteObject(hdc);
+					}
+
+				}
+			}
+			GetClientRect(hWnd, lprect);
+			hPen = CreatePen(PS_SOLID, 5, RGB(cfg.color_line.r, cfg.color_line.g, cfg.color_line.b));
+			SelectObject(hdc, hPen);
+
+			for (int i = 0; i <= cfg.n; i++)
+			{
+				MoveToEx(hdc, lprect->left + a * i, lprect->top, 0);
+				LineTo(hdc, lprect->left + a * i, lprect->bottom);
 
 			}
-		}
-		GetClientRect(hWnd, lprect);
-		hPen = CreatePen(PS_SOLID, 5, RGB(cfg.color_line.r, cfg.color_line.g, cfg.color_line.b));
-		SelectObject(hdc, hPen);
+			for (int i = 1; i <= cfg.n; i++)
+			{
 
-		for (int i = 0; i <= cfg.n; i++)
-		{
-			MoveToEx(hdc,	lprect->left + a * i,	lprect->top,	0);
-			LineTo	(hdc,	lprect->left + a * i,	lprect->bottom);
+				MoveToEx(hdc, lprect->left, lprect->top + b * i, 0);
+				LineTo(hdc, lprect->right, lprect->top + b * i);
 
-		}
-		for (int i = 1; i <= cfg.n; i++)
-		{
+			}
 
-			MoveToEx(hdc,	lprect->left,	lprect->top + b * i,	0);
-			LineTo	(hdc,	lprect->right,	lprect->top + b * i);
-
-		}
-
-		DeleteObject(hPen);
-		DeleteObject(hdc);
-		EndPaint(hWnd, &ps);
-		return 0;
-
-	case WM_DESTROY:
-		GetWindowRect(hWnd, lprect);
-		cfg.width = lprect->right - lprect->left;
-		cfg.height = lprect->bottom - lprect->top;
-		saveConfig(cfg);					// сохраняем конфиг
-
-		PostQuitMessage(0);
-		return 0;
-
-	case WM_MOUSEWHEEL:
-
-		if(GET_WHEEL_DELTA_WPARAM(wParam) > 0)
-			rgbChanger2(cfg.color_line, 5);
-		else
-			rgbChanger2(cfg.color_line, -5);
-		
-		InvalidateRect(hWnd, 0, true);
-		return 0;
-
-	case WM_KEYDOWN:
-
-		printf("%d\n", wParam);
-		if (wParam == VK_RETURN) {				//Enter
-			cfg.color_field.r = rand() % 256;
-			cfg.color_field.g = rand() % 256;
-			cfg.color_field.b = rand() % 256;
-			col = (HBRUSH)SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(cfg.color_field.r, cfg.color_field.g, cfg.color_field.b)));
-			InvalidateRect(hWnd, 0, true);
-			DeleteObject(col);
-		}
-
-		if (wParam == VK_ESCAPE)				//Esc
-		{
-			PostQuitMessage(0);					//Завершаем работу
-			return 0;							//
-		}
-
-		if (wParam == 0x51)						// ctrl + q
-		{
-			if (GetKeyState(VK_CONTROL) < 0)	//
-				PostQuitMessage(0);				//Завершаем работу
+			DeleteObject(hPen);
+			DeleteObject(hdc);
+			EndPaint(hWnd, &ps);
 			return 0;
-		}
 
-		if (wParam == 0x43)						//shift + c
-		{
-			if (GetKeyState(VK_SHIFT) < 0)		//
-				RunNotepad();					//Запускаем блокнот
-			return 0;
-		}
-
-		if (wParam == VK_SPACE)					//space для установки дефолтных значений
-		{
-			cfg.n = 3;
-			cfg.width = 320;
-			cfg.height = 240;
-			cfg.color_line = { 255, 0, 0 };
-			cfg.color_field = { 0, 0, 255 };
-			col = (HBRUSH)SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(cfg.color_field.r, cfg.color_field.g, cfg.color_field.b)));
-			DeleteObject(col);
+		case WM_DESTROY:
 			GetWindowRect(hWnd, lprect);
-			MoveWindow(hWnd,lprect->left, lprect->top, cfg.width, cfg.height, TRUE); //->Move window
-			for (int i = 0; i < cfg.n; i++)
-				for (int j = 0; j < cfg.n; j++)
-					array[i][j] = 0;
+			cfg.width = lprect->right - lprect->left;
+			cfg.height = lprect->bottom - lprect->top;
+			saveConfig(cfg);					// сохраняем конфиг
+
+			PostQuitMessage(0);
+			return 0;
+
+		case WM_MOUSEWHEEL:
+
+			if (GET_WHEEL_DELTA_WPARAM(wParam) > 0)
+				rgbChanger2(cfg.color_line, 5);
+			else
+				rgbChanger2(cfg.color_line, -5);
+
 			InvalidateRect(hWnd, 0, true);
+			return 0;
+
+		case WM_KEYDOWN:
+
+			printf("%d\n", wParam);
+			if (wParam == VK_RETURN) {				//Enter
+				cfg.color_field.r = rand() % 256;
+				cfg.color_field.g = rand() % 256;
+				cfg.color_field.b = rand() % 256;
+				col = (HBRUSH)SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(cfg.color_field.r, cfg.color_field.g, cfg.color_field.b)));
+				InvalidateRect(hWnd, 0, true);
+				DeleteObject(col);
+			}
+
+			if (wParam == VK_ESCAPE)				//Esc
+			{
+				PostQuitMessage(0);					//Завершаем работу
+				return 0;							//
+			}
+
+			if (wParam == 0x51)						// ctrl + q
+			{
+				if (GetKeyState(VK_CONTROL) < 0)	//
+					PostQuitMessage(0);				//Завершаем работу
+				return 0;
+			}
+
+			if (wParam == 0x43)						//shift + c
+			{
+				if (GetKeyState(VK_SHIFT) < 0)		//
+					RunNotepad();					//Запускаем блокнот
+				return 0;
+			}
+
+			if (wParam == VK_SPACE)					//space для установки дефолтных значений
+			{
+				cfg.n = 3;
+				cfg.width = 320;
+				cfg.height = 240;
+				cfg.color_line = { 255, 0, 0 };
+				cfg.color_field = { 0, 0, 255 };
+				col = (HBRUSH)SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(cfg.color_field.r, cfg.color_field.g, cfg.color_field.b)));
+				DeleteObject(col);
+				GetWindowRect(hWnd, lprect);
+				MoveWindow(hWnd, lprect->left, lprect->top, cfg.width, cfg.height, TRUE); //->Move window
+				for (int i = 0; i < cfg.n; i++)
+					for (int j = 0; j < cfg.n; j++)
+						array[i][j] = 0;
+				InvalidateRect(hWnd, 0, true);
+
+				return 0;
+			}
 
 			return 0;
+
 		}
-
-		return 0;
-
 	}
 	return DefWindowProc(hWnd, message, wParam, lParam);
 }
@@ -361,18 +388,24 @@ LRESULT CALLBACK WndProc(
 
 int main(int argc, char* argv[])
 {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	setlocale(LOCALE_ALL, "ru");
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_INTENSITY);
+	FreeConsole();
+
+	/*if (EnumWindows(&EnumWindowsProc, 0))
+	{
+		std::cout << "EnumWindows - ok" << std::endl;
+	}
+	else { std::cout << "Error EnumWindows..." << std::endl; }*/
+
 	srand(time(NULL));
 	loadConfig(cfg);
 
 	if (argc > 1)
 	{
 		cfg.n = atoi(argv[1]);
-	}
-
-	array = new int* [cfg.n];
-	for (int i = 0; i < cfg.n; i++)
-	{
-		array[i] = new int [cfg.n];
 	}
 
 	HINSTANCE	hThis	= GetModuleHandle(NULL);
@@ -383,12 +416,12 @@ int main(int argc, char* argv[])
 
 	if (!RegisterClass(&cls))
 	{
-		printf("%A\n", GetLastError());
+		printf("%d\n", GetLastError());
 	}
 
 	HWND hWnd = CreateWindow(
 		szClsName,
-		_T("Hello Window"),
+		_T("HelloWindow"),
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
@@ -402,6 +435,56 @@ int main(int argc, char* argv[])
 	(HBRUSH)SetClassLongPtr(hWnd, GCLP_HBRBACKGROUND, (LONG_PTR)CreateSolidBrush(RGB(cfg.color_field.r, cfg.color_field.g, cfg.color_field.b)));
 	ShowWindow(hWnd, SW_SHOW);
 
+	//////////////////////////////////////////////////////////////////////////////////////////////
+	HANDLE	hMapFile;
+
+	hMapFile = CreateFileMapping(
+		INVALID_HANDLE_VALUE,				// без файла на диске
+		NULL,								// не наследуется процессами-потомками
+		PAGE_READWRITE,
+		0,
+		sizeof(array) * cfg.n * cfg.n,		// размер максимальный
+		_T("TestMapping"));		// имя
+
+	if (hMapFile == NULL)
+	{
+		return printf("CreateFileMapping error: %d\n", GetLastError());
+	}
+
+	int* pBuf = (int*)MapViewOfFile(hMapFile,	// handle to map object
+		FILE_MAP_READ | FILE_MAP_WRITE,		// read/write permission
+		0,
+		0,
+		0);
+
+	if (pBuf == NULL)
+	{
+		printf("Error mapping file data: %d\n", GetLastError());
+
+		CloseHandle(hMapFile);
+
+		return 1;
+	}
+
+	if (GetLastError() != ERROR_ALREADY_EXISTS) {
+		printf("Hello i`m creator\n");
+	}
+
+	if (GetLastError() == ERROR_ALREADY_EXISTS) {
+		printf("Hello i`m listener\n");
+	}
+
+	array = new int* [cfg.n];
+	for (int i = 0; i < cfg.n; i++)
+	{
+		array[i] = pBuf + cfg.n * i;
+	}
+
+	CopyMemory((PVOID)pBuf, array, sizeof(array) * cfg.n * cfg.n);
+
+
+	std::cout << hWnd << "\n";
+	//////////////////////////////////////////////////////////////////////////////////////////////
 
 	BOOL bOk;
 	MSG msg;
@@ -410,13 +493,16 @@ int main(int argc, char* argv[])
 	{
 		if (bOk < 0)
 		{
-			printf("%A\n", GetLastError());
+			printf("%d\n", GetLastError());
 			break;
 		}
 
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
+
+	UnmapViewOfFile(pBuf);
+	CloseHandle(hMapFile);
 
 	DestroyWindow(hWnd);
 	UnregisterClass(szClsName, hThis);
